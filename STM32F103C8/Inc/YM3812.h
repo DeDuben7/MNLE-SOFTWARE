f8c0c4825a1dabf5e6444a722a3b22016a023d2d
Ruben van Renswoude <ruben@vanrenswoude.info>
2019-10-25 11:36:11 +0200

    completed defines for ym

--- STM32F103C8/Inc/YM3812.h
+++ STM32F103C8/Inc/YM3812.h
@@ -10,20 +10,20 @@
 
 #include "includes.h"
 
-#define A0 GPIO_PIN
-#define WR GPIO_PIN
-#define RST GPIO_PIN
-#define CS1 GPIO_PIN
-#define CS2 GPIO_PIN
-#define CS3 GPIO_PIN
-#define CS4 GPIO_PIN
-#define D0 GPIO_PIN
-#define D1 GPIO_PIN
-#define D2 GPIO_PIN
-#define D3 GPIO_PIN
-#define D4 GPIO_PIN
-#define D5 GPIO_PIN
-#define D6 GPIO_PIN
-#define D7 GPIO_PIN
+#define A0 GPIO_PIN_8 //GPIOB
+#define WR GPIO_PIN_9 //GPIOB
+#define RST GPIO_PIN_8 //GPIOA
+#define CS1 GPIO_PIN_12 //GPIOA
+#define CS2 GPIO_PIN_11 //GPIOA
+#define CS3 GPIO_PIN_10 //GPIOA
+#define CS4 GPIO_PIN_9 //GPIOA
+#define D0 GPIO_PIN_13 //GPIOA
+#define D1 GPIO_PIN_14 //GPIOA
+#define D2 GPIO_PIN_15 //GPIOA
+#define D3 GPIO_PIN_3 //GPIOB
+#define D4 GPIO_PIN_4 //GPIOB
+#define D5 GPIO_PIN_5 //GPIOB
+#define D6 GPIO_PIN_6 //GPIOB
+#define D7 GPIO_PIN_7 //GPIOB
 
 #endif /* YM3812_H_ */
