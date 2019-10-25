9c872d37fea13374224b8adea262129f21832980
Ruben van Renswoude <ruben@vanrenswoude.info>
2019-10-25 10:18:15 +0200

    Added YM3812 h and c files

---
+++ STM32F103C8/Inc/YM3812.h
@@ -0,0 +1,13 @@
+/*
+ * YM3812.h
+ *
+ *  Created on: 25 okt. 2019
+ *      Author: Ruben
+ */
+
+#ifndef YM3812_H_
+#define YM3812_H_
+
+
+
+#endif /* YM3812_H_ */
---
+++ STM32F103C8/Src/YM3812.c
@@ -0,0 +1,4 @@
+#include "includes.h"
+
+uint8_t YM_WRITE_ADDR(uint8_t addr);
+uint8_t YM_WRITE_DATA(uint8_t data);
