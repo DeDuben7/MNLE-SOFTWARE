bfcbfe0c1d0c3eef88f1693f0c97bb0fc5856df2
Ruben van Renswoude <ruben@vanrenswoude.info>
2019-10-25 09:47:13 +0200

    initial commit

---
+++ STM32F103C8/Src/MIDI.c
@@ -0,0 +1,84 @@
+/*
+*********************************************************************************************************
+*
+* Filename      : MIDI.C
+* Version       : V1.00
+* Programmer(s) : Julian Schotborgh
+*
+*  21-10-19 Opstart midi.c en maken midi_proc()
+*********************************************************************************************************
+*/
+
+#include "includes.h"
+
+
+void MIDI_PROC(uint8_t MIDI_DATA[2],uint8_t buflen)
+{
+	uint8_t MIDI_STATUS; // binnengekomen status byte
+	uint8_t FUNC_VAR[1]; // binnengekomen data voor de functies
+	char MIDI_FUNC; // variabele om fucntie nibble op te slaan
+	char MIDI_CHANNEL; // variabele om channel nibble op te slaan
+	int err = 0; // error variabele
+
+
+
+	switch (buflen)
+	{
+		case 2:
+			MIDI_STATUS = MIDI_DATA[0];
+			MIDI_FUNC = (char)(MIDI_STATUS & 0x0F); // lsb bits van byte omzetten in nibble
+			MIDI_CHANNEL = (char)((MIDI_STATUS & 0x0F)>>4); // msb bits van byte omzetten in nibble
+
+			for (int i=0;i<1;i++) // data halen uit binnengekomen bericht
+			{
+				FUNC_VAR[i] = MIDI_DATA[i+1];
+			}
+
+			switch(MIDI_FUNC) // switch om data naar goeie YM functie te sturen
+			{
+				case PROG_CHANGE:
+					//err = YM_PROG_CHANGE(MIDI_CHANNEL,FUNC_VAR[0]);
+					break;
+				case PITCH:
+					//err = YM_PITCH(MIDI_CHANNEL,FUNC_VAR[1]);
+					break;
+			}
+
+			break;
+		case 3:
+
+			MIDI_STATUS = MIDI_DATA[0];
+			MIDI_FUNC = (char)((MIDI_STATUS & 0xF0) >> 4); // lsb bits van byte omzetten in nibble
+			MIDI_CHANNEL = (char)(MIDI_STATUS & 0x0F); // msb bits van byte omzetten in nibble
+
+			for (int i=0;i<2;i++) // data halen uit binnengekomen bericht
+			{
+				FUNC_VAR[i] = MIDI_DATA[i+1];
+			}
+
+			switch(MIDI_FUNC) // switch om data naar goeie YM functie te sturen
+			{
+				case NOTE_OFF:
+					//err = YM_NOTE_ON(MIDI_CHANNEL,FUNC_VAR[0],0);
+					//HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
+					//HAL_UART_Transmit(&huart2, "noteoff", 7, 100);
+					break;
+				case NOTE_ON:
+					//err = YM_NOTE_ON(MIDI_CHANNEL,FUNC_VAR[0],FUNC_VAR[1]);
+					//HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
+					//HAL_UART_Transmit_IT(&huart2, "noteon", 6);
+					break;
+				case CONT_CHANGE:
+					//err = YM_CONT_CHANGE(MIDI_CHANNEL,FUNC_VAR[0],FUNC_VAR[1]);
+					//HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
+					//HAL_UART_Transmit(&huart2, "cont", 4, 100);
+					break;
+				default:
+					return;
+			}
+
+			break;
+		default:
+			return;
+	}
+}
---
+++ STM32F103C8/Src/main.c
@@ -0,0 +1,533 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2019 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+
+/* USER CODE BEGIN PV */
+uint8_t data;
+uint8_t buf1[3];
+uint8_t buf2[3];
+uint8_t buflen;
+uint8_t buf1len;
+uint8_t buf2len;
+uint8_t BUF1_FLAG = FALSE;
+uint8_t BUF2_FLAG = FALSE;
+uint8_t BUF1_FULL = FALSE;
+uint8_t BUF2_FULL = FALSE;
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+void GPIO_Init(void);
+static void MX_GPIO_Init(void);
+static void MX_ADC1_Init(void);
+static void MX_I2C2_Init(void);
+static void MX_SPI2_Init(void);
+static void MX_USART2_UART_Init(void);
+
+/* USER CODE BEGIN PFP */
+uint8_t byte = 0;
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+
+  GPIO_Init();
+  MX_ADC1_Init();
+  MX_I2C2_Init();
+  MX_SPI2_Init();
+  MX_USART2_UART_Init();
+
+  /* USER CODE BEGIN 2 */
+
+  HAL_UART_Receive_IT(&huart2, &data, 1);
+
+  while (1)
+  {
+	  if(BUF1_FULL)
+	  {
+		  MIDI_PROC(buf1, buf1len);
+		  HAL_UART_Transmit_IT(&huart2, buf1, 3);
+		  BUF1_FULL = FALSE;
+	  }
+
+	  if(BUF2_FULL)
+	  {
+		  MIDI_PROC(buf2, buf2len);
+		  HAL_UART_Transmit_IT(&huart2, buf2, 3);
+		  BUF2_FULL = FALSE;
+	  }
+
+  }
+}
+
+/**
+  * @brief UART interrupt handler
+  * @retval None
+  */
+void USART2_IRQHandler(void)
+{
+	HAL_UART_IRQHandler(&huart2);
+}
+
+/* This callback is called by the HAL_UART_IRQHandler when the given number of bytes are received */
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
+{
+	if (huart->Instance == USART2)
+	{
+		//HAL_UART_Transmit_IT(&huart2, &data, 1);
+		if(data & 128) //check if new data block is received
+		{
+			if((BUF1_FLAG != 1) && (BUF2_FLAG != 1)) //first time
+			{
+				HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
+				BUF1_FLAG = TRUE;
+				buflen = 0;
+				buf1[buflen++] = data;
+			}
+
+			else if((BUF1_FLAG != 1) && (BUF2_FLAG == 1)) //write new data to buf1 and send buf2 to MIDI_CONT
+			{
+				BUF2_FULL = TRUE;
+				buf2len = buflen;
+				BUF1_FLAG = TRUE;
+				BUF2_FLAG = FALSE;
+				buflen = 0;
+				buf1[buflen++] = data;
+			}
+
+			else if((BUF1_FLAG == 1) && (BUF2_FLAG != 1)) //write new data to buf2 and send buf1 to MIDI_CONT
+			{
+				BUF1_FULL = TRUE;
+				buf1len = buflen;
+				BUF1_FLAG = FALSE;
+				BUF2_FLAG = TRUE;
+				buflen = 0;
+				buf2[buflen++] = data;
+			}
+		}
+
+		else
+		{
+			if((BUF1_FLAG != 1) && (BUF2_FLAG == 1)) //write data to buf2
+				buf2[buflen++] = data;
+
+			else if((BUF1_FLAG == 1) && (BUF2_FLAG != 1)) //write data to buf1
+				buf1[buflen++] = data;
+		}
+
+		HAL_UART_Receive_IT(&huart2, &data, 1);
+	}
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+  RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
+
+  /** Initializes the CPU, AHB and APB busses clocks
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Initializes the CPU, AHB and APB busses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_ADC;
+  PeriphClkInit.AdcClockSelection = RCC_ADCPCLK2_DIV2;
+  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief ADC1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_ADC1_Init(void)
+{
+
+  /* USER CODE BEGIN ADC1_Init 0 */
+  // hadc1 struct voor oproepen ADC
+  /* USER CODE END ADC1_Init 0 */
+
+  ADC_ChannelConfTypeDef sConfig = {0};
+
+  /* USER CODE BEGIN ADC1_Init 1 */
+
+  /* USER CODE END ADC1_Init 1 */
+  /** Common config
+  */
+  hadc1.Instance = ADC1;
+  hadc1.Init.ScanConvMode = ADC_SCAN_DISABLE;
+  hadc1.Init.ContinuousConvMode = DISABLE;
+  hadc1.Init.DiscontinuousConvMode = DISABLE;
+  hadc1.Init.ExternalTrigConv = ADC_SOFTWARE_START;
+  hadc1.Init.DataAlign = ADC_DATAALIGN_RIGHT;
+  hadc1.Init.NbrOfConversion = 1;
+  if (HAL_ADC_Init(&hadc1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Configure Regular Channel
+  */
+  sConfig.Channel = ADC_CHANNEL_4;
+  sConfig.Rank = ADC_REGULAR_RANK_1;
+  sConfig.SamplingTime = ADC_SAMPLETIME_1CYCLE_5;
+  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN ADC1_Init 2 */
+
+  /* USER CODE END ADC1_Init 2 */
+
+}
+
+/**
+  * @brief I2C2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_I2C2_Init(void)
+{
+
+  /* USER CODE BEGIN I2C2_Init 0 */
+
+  /* USER CODE END I2C2_Init 0 */
+
+  /* USER CODE BEGIN I2C2_Init 1 */
+
+  /* USER CODE END I2C2_Init 1 */
+  hi2c2.Instance = I2C2;
+  hi2c2.Init.ClockSpeed = 400000;
+  hi2c2.Init.DutyCycle = I2C_DUTYCYCLE_2;
+  hi2c2.Init.OwnAddress1 = 0;
+  hi2c2.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+  hi2c2.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+  hi2c2.Init.OwnAddress2 = 0;
+  hi2c2.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+  hi2c2.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+  if (HAL_I2C_Init(&hi2c2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN I2C2_Init 2 */
+
+  /* USER CODE END I2C2_Init 2 */
+
+}
+
+/**
+  * @brief SPI2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_SPI2_Init(void)
+{
+
+  /* USER CODE BEGIN SPI2_Init 0 */
+
+  /* USER CODE END SPI2_Init 0 */
+
+  /* USER CODE BEGIN SPI2_Init 1 */
+
+  /* USER CODE END SPI2_Init 1 */
+  /* SPI2 parameter configuration*/
+  hspi2.Instance = SPI2;
+  hspi2.Init.Mode = SPI_MODE_MASTER;
+  hspi2.Init.Direction = SPI_DIRECTION_2LINES;
+  hspi2.Init.DataSize = SPI_DATASIZE_8BIT;
+  hspi2.Init.CLKPolarity = SPI_POLARITY_LOW;
+  hspi2.Init.CLKPhase = SPI_PHASE_1EDGE;
+  hspi2.Init.NSS = SPI_NSS_SOFT;
+  hspi2.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_2;
+  hspi2.Init.FirstBit = SPI_FIRSTBIT_MSB;
+  hspi2.Init.TIMode = SPI_TIMODE_DISABLE;
+  hspi2.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
+  hspi2.Init.CRCPolynomial = 10;
+  if (HAL_SPI_Init(&hspi2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN SPI2_Init 2 */
+
+  /* USER CODE END SPI2_Init 2 */
+
+}
+
+/**
+  * @brief USART2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART2_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART2_Init 0 */
+
+  /* USER CODE END USART2_Init 0 */
+
+  /* USER CODE BEGIN USART2_Init 1 */
+
+  /* USER CODE END USART2_Init 1 */
+  huart2.Instance = USART2;
+  huart2.Init.BaudRate = 32500; //Baudrate of MIDI messages is 32500 baud
+  huart2.Init.WordLength = UART_WORDLENGTH_8B; //Wordlenght is 8b
+  huart2.Init.StopBits = UART_STOPBITS_1; //There will be one stopbit
+  huart2.Init.Parity = UART_PARITY_NONE; //There is no paritybit
+  huart2.Init.Mode = UART_MODE_TX_RX; //Use RX for receiving MIDI and TX for sending messages to serial monitor via Arduino
+  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  HAL_NVIC_SetPriority(USART2_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(USART2_IRQn);
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOD_CLK_ENABLE();
+}
+
+/* USER CODE BEGIN 4 */
+void GPIO_Init()
+{
+	MX_GPIO_Init();
+	GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+	// ADC1_IN0
+	GPIO_InitStruct.Pin = GPIO_PIN_0;
+	GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
+	GPIO_InitStruct.Pull = GPIO_NOPULL;
+	HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+	// ADC1_IN1
+	GPIO_InitStruct.Pin = GPIO_PIN_1;
+	GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
+	GPIO_InitStruct.Pull = GPIO_NOPULL;
+	HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+	// ADC1_IN4
+	GPIO_InitStruct.Pin = GPIO_PIN_4;
+	GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
+	GPIO_InitStruct.Pull = GPIO_NOPULL;
+	HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+	// ADC1_IN5
+	GPIO_InitStruct.Pin = GPIO_PIN_5;
+	GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
+	GPIO_InitStruct.Pull = GPIO_NOPULL;
+	HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+
+	// ADC1_IN6
+	GPIO_InitStruct.Pin = GPIO_PIN_6;
+	GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
+	GPIO_InitStruct.Pull = GPIO_NOPULL;
+	HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+	// ADC1_IN7
+	GPIO_InitStruct.Pin = GPIO_PIN_7;
+	GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
+	GPIO_InitStruct.Pull = GPIO_NOPULL;
+	HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+	// ADC1_IN8
+	GPIO_InitStruct.Pin = GPIO_PIN_0;
+	GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
+	GPIO_InitStruct.Pull = GPIO_NOPULL;
+	HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+
+	// ADC1_IN9
+	GPIO_InitStruct.Pin = GPIO_PIN_1;
+	GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
+	GPIO_InitStruct.Pull = GPIO_NOPULL;
+	HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+
+	//I2C_CLOCK
+	GPIO_InitStruct.Pin = GPIO_PIN_10;
+	GPIO_InitStruct.Mode = GPIO_MODE_AF_OD;
+	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+	GPIO_InitStruct.Pull = GPIO_NOPULL;
+	HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+
+	//I2C_DATA
+	GPIO_InitStruct.Pin = GPIO_PIN_11;
+	GPIO_InitStruct.Mode = GPIO_MODE_AF_OD;
+	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+	GPIO_InitStruct.Pull = GPIO_NOPULL;
+	HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+
+	//SPI
+	GPIO_InitStruct.Pin = GPIO_PIN_15 || GPIO_PIN_14 || GPIO_PIN_13;
+	GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+	GPIO_InitStruct.Pull = GPIO_NOPULL;
+	HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+
+	//YM3812 PINS PART 1
+	GPIO_InitStruct.Pin = GPIO_PIN_2|| GPIO_PIN_3|| GPIO_PIN_4|| GPIO_PIN_5|| GPIO_PIN_6|| GPIO_PIN_7|| GPIO_PIN_8||GPIO_PIN_9||GPIO_PIN_12;
+	GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+	GPIO_InitStruct.Pull = GPIO_PULLDOWN;
+	HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+	//YM3812 PINS PART 2
+	GPIO_InitStruct.Pin = GPIO_PIN_8||GPIO_PIN_9||GPIO_PIN_10||GPIO_PIN_11|| GPIO_PIN_12|| GPIO_PIN_13|| GPIO_PIN_14||GPIO_PIN_15;
+	GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+	GPIO_InitStruct.Pull = GPIO_PULLDOWN;
+	HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+
+	//LED PIN
+	GPIO_InitStruct.Pin = GPIO_PIN_13;
+	GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+	GPIO_InitStruct.Pull = GPIO_PULLDOWN;
+	HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+}
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     tex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
